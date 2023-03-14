2beba385b18492e6404d05434f73e17f89136747
icika-max <icikakennedy@gmail.com>
2023-03-14 14:48:40 +0300

    completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  resources :plants, only: [:index, :show, :create]
+
+  # get '/plants', to: 'plants#index'
+  # get '/plants/:id', to: 'plants#show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
